6ae399d223a1ed9faa14ac8a889f117ffb0bb3b5
DKRVISSION <dkrvission88@gmail.com>
2024-02-13 09:23:28 +0530

    categorysend

---
+++ src/App.js
@@ -0,0 +1,38 @@
+// import Card01 from "./Cards/Card01";
+import Catogary from "./Components/Catogary";
+// import PrivacyP from "./Components/PrivacyP";
+
+// import Send from "./Components/POPup/Send";
+
+// import Terms from "./Components/Terms";
+
+// import Search from "./Components/Search";
+
+// import {}
+
+
+function App() {
+  return (
+
+    <>
+    {/* <Send/> */}
+
+    {/* <Terms/> */}
+
+
+
+    {/* <PrivacyP/> */}
+
+    <Catogary/>
+
+    {/* <Search/> */}
+
+    {/* <Card01/> */}
+    
+    
+    </>
+    
+  );
+}
+
+export default App;
